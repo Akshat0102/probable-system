0a0775cea0826eb3fc6cb68f48b497c9c311c16b
amangangwar <202051020@iiitvadodara.ac.in>
2023-01-18 15:29:30 +0530

    Added java file for jenkins execution

---
+++ aws_jenkins/jenkins_script.java
@@ -0,0 +1,5 @@
+class jenkins_script {
+    public static void main(String[] args) {
+        System.out.println("Hello!, This is the Java file created by Aman Gangwar (202051020) and executed using Jenkins on an AWS EC2 instance :)");
+    }
+}
