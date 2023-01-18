599bb4a1a6688e204bc02092f82c3a9763d9b296
iamangangwar <amangangwar084@gmail.com>
2023-01-18 15:45:02 +0530

    update

--- aws_jenkins/jenkins_script.java
+++ aws_jenkins/jenkins_script.java
@@ -1,4 +1,4 @@
-class jenkins_script {
+public class jenkins_script {
     public static void main(String[] args) {
         System.out.println("Hello!, This is the Java file created by Aman Gangwar (202051020) and executed using Jenkins on an AWS EC2 instance :)");
     }
