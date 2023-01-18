b6efe29465a7414b01dda12390c4e11357abf331
iamangangwar <amangangwar084@gmail.com>
2023-01-18 16:40:34 +0530

    jenkins test repo update

--- jenkins_script.java
+++ jenkins_script.java
@@ -1,5 +1,6 @@
 public class jenkins_script {
     public static void main(String[] args) {
-        System.out.println("Hello!, This is the Java file created by Aman Gangwar (202051020) and executed using Jenkins on an AWS EC2 instance :)");
+        System.out.println("Hello!, This is the Java file created by Aman Gangwar (202051020) and executed using Jenkins on an AWS EC2 instance :)\n
+                            This message updates the repo.");
     }
 }
